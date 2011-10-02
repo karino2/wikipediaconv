6078dc38bb9561cf1419e6f1c55cb544469dbb11
karino <karino@localhost>
2011-10-02 11:46:33 +0900

    Implement foundation of SplitFolder.

--- WikipediaConv/SplitFolder.cs
+++ WikipediaConv/SplitFolder.cs
@@ -2,13 +2,124 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
 
 namespace WikipediaConv
 {
-    class SplitFolder
+    public class SplitFolder
     {
-        public void Hello()
+        public virtual FileInfo[] Files
         {
+            get
+            {
+                return Current.GetFiles();
+            }
         }
+
+        DirectoryInfo Base { get; set; }
+        DirectoryInfo Current { get; set; }
+        public int UpperFileLimit { get; set; }
+
+
+        public SplitFolder(DirectoryInfo baseDi)
+            : this(baseDi, baseDi)
+        { }
+
+        public SplitFolder(DirectoryInfo baseDi, DirectoryInfo current)
+        {
+            Base = baseDi;
+            Current = current;
+        }
+
+        public bool NeedSplit
+        {
+            get
+            {
+                return Files.Length >= UpperFileLimit;
+            }
+        }
+
+        public void Split()
+        {
+            if (SubFolderExists)
+                throw new Exception("Already splited");
+            CreateSubdirectories();
+            SortToSubdirectories();
+        }
+
+        internal virtual void MoveTo(FileInfo target, string destPath)
+        {
+            target.MoveTo(destPath);
+        }
+
+        private void SortToSubdirectories()
+        {
+            foreach (var file in Files)
+            {
+                string dest = GetMatchedSubdirectoryPath(file);
+                if(dest != Current.FullName)
+                    MoveTo(file, Path.Combine(dest, file.Name));
+            }
+        }
+
+        internal string FileNameHeadUntilCurrent
+        {
+            get
+            {
+                if(Base == Current)
+                    return "";
+                List<String> dirs = new List<String>();
+                DirectoryInfo di = Current;
+                while (Base.FullName != di.FullName)
+                {
+                    dirs.Add(di.Name);
+                    di = di.Parent;
+                }
+                dirs.Reverse();
+                StringBuilder bldr = new StringBuilder();
+                dirs.ForEach((d) => bldr.Append(d));
+                return bldr.ToString();
+            }
+        }
+
+        internal string GetMatchedSubdirectoryPath(FileInfo file)
+        {
+            string untilCur = FileNameHeadUntilCurrent;
+            if (file.Name == untilCur)
+                return untilCur;
+            Debug.Assert(file.Name.StartsWith(untilCur));
+            string nextHead = file.Name.Substring(untilCur.Length, 1);
+            if(InsideNormalRange(nextHead))
+                return Path.Combine(Current.FullName, file.Name.Substring(0, untilCur.Length + 1));
+            throw new Exception("NYI: special character handling.");
+        }
+
+        private bool InsideNormalRange(string nextHead)
+        {
+            return nextHead[0] >= 'a' && nextHead[0] <= 'z';
+        }
+
+        void CreateSubdirectories()
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                CreateSubdirectory(c);
+            }
+        }
+
+        internal virtual void CreateSubdirectory(char c)
+        {
+            Current.CreateSubdirectory(Path.Combine(Current.FullName, c.ToString()));
+        }
+
+        public bool SubFolderExists
+        {
+            get
+            {
+                return Current.GetDirectories().Length != 0;
+            }
+        }
+
     }
 }
--- WikipediaConvTest/SplitFolderTest.cs
+++ WikipediaConvTest/SplitFolderTest.cs
@@ -7,15 +7,38 @@ namespace WikipediaConvTest
 {
     using NUnit.Framework;
     using WikipediaConv;
-
+    using System.IO;
+    
     [TestFixture]
     public class SplitFolderTest
     {
+
         [Test]
-        public void TestHello()
+        public void TestGetFileNameHeadUntilCurrent_Root()
         {
-            SplitFolder sf = new SplitFolder();
-            sf.Hello();
+            string expected = "";
+            SplitFolder sf = new SplitFolder(new DirectoryInfo(@"I:\hoge"));
+            string actual = sf.FileNameHeadUntilCurrent;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestGetFileNameHeadUntilCurrent_TwoLevel()
+        {
+            string expected = "ika";
+            SplitFolder sf = new SplitFolder(new DirectoryInfo(@"I:\hoge"), new DirectoryInfo(@"I:\hoge\i\k\a"));
+            string actual = sf.FileNameHeadUntilCurrent;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void TestGetMatchedSubdirectoryPath_Root()
+        {
+            string expected = @"I:\hoge\i";
+            SplitFolder sf = new SplitFolder(new DirectoryInfo(@"I:\hoge"));
+            string actual = sf.GetMatchedSubdirectoryPath(new FileInfo(@"I:hoge\ika.txt"));
+            Assert.AreEqual(expected, actual);
         }
     }
 }
